cbffa2ef76b7c258fdef21f976e83c009571e308
RITUL  RAJ <ritulraj1000@gmail.com>
2024-07-23 04:30:37 +0530

    final

--- Backend/routes/Post.js
+++ Backend/routes/Post.js
@@ -5,20 +5,14 @@ import { LikeandUnlikePost } from "../Controllers/Post.js";
 import { DeletePost } from "../Controllers/Post.js";
 const router=express.Router();
 
-// router.post("/post/upload",isAuthenticated,createPost)
-// router.get("/post/:id",isAuthenticated,LikeandUnlikePost)
-// router.put("/post/:id",isAuthenticated,updateCaption)
-// router.delete("/post/:id",isAuthenticated,DeletePost)
-// router.get("/posts",isAuthenticated,getPostOfFollowing)
-// router.put("/post/comment/:id",isAuthenticated,addComment)
-// router.delete("/post/comment/:id",isAuthenticated,deleteComment)
-router.post("/post/upload",createPost)
-router.get("/post/:id",LikeandUnlikePost)
-router.put("/post/:id",updateCaption)
-router.delete("/post/:id",DeletePost)
-router.get("/posts",getPostOfFollowing)
-router.put("/post/comment/:id",addComment)
-router.delete("/post/comment/:id",deleteComment)
+router.post("/post/upload",isAuthenticated,createPost)
+router.get("/post/:id",isAuthenticated,LikeandUnlikePost)
+router.put("/post/:id",isAuthenticated,updateCaption)
+router.delete("/post/:id",isAuthenticated,DeletePost)
+router.get("/posts",isAuthenticated,getPostOfFollowing)
+router.put("/post/comment/:id",isAuthenticated,addComment)
+router.delete("/post/comment/:id",isAuthenticated,deleteComment)
+
 
 export default router
 
